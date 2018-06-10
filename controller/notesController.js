ea11847db8b0cb9287363f751522a689d8537ba4
Vimlesh22 <kumarvimlesh007@gmail.com>
2018-06-10 17:05:38 +0530

    comments added to notesController

--- controller/notesController.js
+++ controller/notesController.js
@@ -1,3 +1,19 @@
+/******************************************************************************
+ *  Purpose         : Defines ths controller for the ToDo Notes Application.
+ * 
+ *  @description    
+ * 
+ *  @file           : notesController.js
+ *  @overview       : NotesController class handles all the requests coming from client for various operations delegating controls to specific methods.
+ *  @author         : Vimlesh Kumar <kumarvimlesh007@gmail.com>
+ *  @version        : 1.0
+ *  @since          : 17-05-2018
+ *
+ ******************************************************************************/
+/**
+ * @description Dependencies require to be installed before the execution of this file.
+ * @const {Class} notesService class instance of the notesService
+ */
 const notesService = require('../service/notesService');
 
 function NotesController()
@@ -6,7 +22,11 @@ function NotesController()
 };
 
 
-
+/**
+ * @description Prototype property adding the property functions.
+ * 
+ * @method createNote() - Creates a note for particular user ,adding all the details about the notes
+ */
 NotesController.prototype.createNote = (req,res,next) => {
   var title = req.body.title;
   var description = req.body.description;
@@ -25,6 +45,11 @@ NotesController.prototype.createNote = (req,res,next) => {
   });
 };
 
+/**
+ * @description Prototype property adding the property functions.
+ * 
+ * @method getNote() - Getting the details of all the available notes to the user.
+ */
 NotesController.prototype.getNote = (req,res,next) => {
   notesService.getNotesService((err,result) => {
     if(err){
@@ -39,6 +64,11 @@ NotesController.prototype.getNote = (req,res,next) => {
   });
 };
 
+/**
+ * @description Prototype property adding the property functions.
+ * 
+ * @method updateNote() - Update the notes.
+ */
 NotesController.prototype.updateNote = (req,res,next) => {
   var id = req.body._id;
   var title = req.body.title;
@@ -55,6 +85,11 @@ NotesController.prototype.updateNote = (req,res,next) => {
   });
 };
 
+/**
+ * @description Prototype property adding the property functions.
+ * 
+ * @method deleteNote() - Deletes a note of particular user.
+ */
 NotesController.prototype.deleteNote = (req,res,next) => {
   var id = req.body.noteId;
   notesService.deleteNoteService(id,(result,err) => {
