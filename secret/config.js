852157cfb852faed22c3261ad68ecff73fd3275c
Vimlesh22 <kumarvimlesh007@gmail.com>
2018-08-28 16:26:10 +0530

    Update config.js

--- secret/config.js
+++ secret/config.js
@@ -2,6 +2,6 @@ module.exports = {
   database : 'mongodb://localhost:27017/ToDoNote',
   secret : 'thisismysecret',
   email : 'kumarvimlesh007@gmail.com',
-  password : 'vimleshd',
+  password : 'shshshs',
   BASE_URL : 'http://localhost:3030/'
 }
